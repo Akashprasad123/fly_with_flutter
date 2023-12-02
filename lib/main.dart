d7193eaa50eb00a195e10a047a19021a063e56c2
Abhijith.U <79012764+abhijithu1@users.noreply.github.com>
2023-12-02 20:12:33 +0530

    curving .

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           primarySwatch: Colors.blue,
           primaryColor: const Color.fromARGB(255, 132, 60, 255)),
-      home: LoginPage(),
+      home: const LoginPage(),
     );
   }
 }
--- lib/scrollview.dart
+++ lib/scrollview.dart
@@ -7,36 +7,34 @@ class ScrollScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("List View"),
+        title: const Text("List View"),
         elevation: 15,
         shadowColor: Colors.black,
         automaticallyImplyLeading: true,
       ),
-      backgroundColor: Color.fromARGB(255, 26, 51, 213),
+      backgroundColor: const Color.fromARGB(255, 26, 51, 213),
       drawer: Drawer(
         backgroundColor: const Color.fromARGB(255, 13, 70, 116),
         elevation: 10,
         shadowColor: Colors.black,
         child: SafeArea(
-          child: Container(
-            child: ListView(
-              children: const [
-                SizedBox(
-                  height: 10,
+          child:  ListView(
+            children: const [
+              SizedBox(
+                height: 10,
+              ),
+              UserAccountsDrawerHeader(
+                accountName: Text("Akash Prasad"),
+                accountEmail: Text(
+                  "abc@example.com",
                 ),
-                UserAccountsDrawerHeader(
-                  accountName: Text("Akash Prasad"),
-                  accountEmail: Text(
-                    "abc@example.com",
-                  ),
-                  currentAccountPicture: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        "https://th.bing.com/th/id/OIP.8sEQq9-fsOY0T-R-vYtVqgHaIB?rs=1&pid=ImgDetMain"),
-                    backgroundColor: Colors.white,
-                  ),
-                )
-              ],
-            ),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://th.bing.com/th/id/OIP.8sEQq9-fsOY0T-R-vYtVqgHaIB?rs=1&pid=ImgDetMain"),
+                  backgroundColor: Colors.white,
+                ),
+              )
+            ],
           ),
         ),
       ),
@@ -51,20 +49,23 @@ class ScrollScreen extends StatelessWidget {
 Widget ScrollView() {
   return ListView.separated(
     itemBuilder: (context, index) {
-      return Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        child: ListTile(
-          minVerticalPadding: 35,
-          leading: CircleAvatar(
-            radius: 35,
-            backgroundColor: Color.fromARGB(255, 5, 3, 134),
+      return Expanded(
+        child: Card(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          child: const ListTile(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(50))),
+            minVerticalPadding: 35,
+            leading: CircleAvatar(
+              radius: 35,
+              backgroundColor: Color.fromARGB(255, 5, 3, 134),
+            ),
+            tileColor: Colors.white,
           ),
-          tileColor: Colors.white,
         ),
       );
     },
     separatorBuilder: (context, index) {
-      return SizedBox(
+      return const SizedBox(
         height: 0,
       );
     },
